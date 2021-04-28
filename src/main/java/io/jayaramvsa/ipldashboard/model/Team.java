6561509ab225df87fba14b6131c82ad38684939e
jayaramvsa <jayaramvsa@gmail.com>
2021-04-28 16:42:35 +0530

    Added Day 03 -Day 04 of IPL dashboard by Kaushik

---
+++ src/main/java/io/jayaramvsa/ipldashboard/model/Team.java
@@ -0,0 +1,36 @@
+package io.jayaramvsa.ipldashboard.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String teamName;
+    private long totalMatches;
+    private long totalWins;
+
+    public Team(String teamName, long totalMatches) {
+        this.teamName = teamName;
+        this.totalMatches = totalMatches;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "teamName='" + teamName + '\'' +
+                ", totalMatches=" + totalMatches +
+                ", totalWins=" + totalWins +
+                '}';
+    }
+}
