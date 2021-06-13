f83c26fc16b2b3bf5e65d493bcdd256e6a64aa6e
jayaramvsa <jayaramvsa@gmail.com>
2021-06-13 18:27:20 +0530

    Completed UI elements for Team Page

---
+++ src/frontend/src/pages/MatchPage.js
@@ -0,0 +1,19 @@
+import { React, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { MatchDetailCard } from '../components/MatchDetailCard.js';
+import { MatchSmallCard } from '../components/MatchSmallCard.js';
+
+
+
+export const MatchPage = () => {
+
+
+
+
+
+    return (
+        <div className="MatchPage">
+            <h1>Match Page</h1>
+        </div>
+    );
+}
